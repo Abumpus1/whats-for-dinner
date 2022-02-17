e0ee5b0b6cf6101f17dca50aa7fa49120d54dbf0
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-02-16 21:49:16 -0700

    Add dynamic meal suggestion output

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 var letsCookBtn = document.querySelector("#cook-button");
 var pot = document.querySelector("#pot-container");
+var mealView = document.querySelector("#meal-view");
+var mealIdea = document.querySelector(".meal-idea");
+var radioButtons = document.querySelectorAll("input");
 
 var sides = ["Rice", "Salad"];
 var mains = ["Spagetti", "Pineapple Pizza"];
 var desserts = ["Banana Bread", "Cheesecake"];
+var meals = [sides, mains, desserts];
 
 letsCookBtn.addEventListener("click", displaySuggestion);
 
 function displaySuggestion() {
+  displayMealIdea();
   toggle(pot);
+  toggle(mealView);
+}
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function displayMealIdea() {
+  // mealIdea.innerText = `${sides[1]}!`;
+  for (var i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      mealIdea.innerText = `${meals[i][getRandomIndex(meals[i])]}!`
+    }
+  }
 }
 
 function toggle(element) {
